9a03c42fec10a80ec5d05419335bd1b731d7ec2d
Christian Adler <chris.adler@gmx.de>
2024-07-02 20:30:52 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js_collatz_conjecture
+
+tut from https://www.youtube.com/watch?v=EYLWxwo1Ed8
---
+++ collatzconjecture.mjs
@@ -0,0 +1,8 @@
+const collatz = (n) => {
+  if (n % 2 === 0)
+    return n / 2;
+  else
+    return n * 3 + 1;
+};
+
+export {collatz};
